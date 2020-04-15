0dd32217e0f33d2c6e140e5b7109fabd0ec96097
Tony López <lopezpagan@gmail.com>
2020-04-15 12:19:48 -0400

    Remove foward to home in the routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadChildren: () => import('./public/home/home.module').then(m => m.HomeModule) },
+    { path: '', loadChildren: () => import('./public/home/home.module').then(m => m.HomeModule) },
     { path: 'error', loadChildren: () => import('./errors/server-error/server-error.module').then(m => m.ServerErrorModule) },
     { path: 'access-denied', loadChildren: () => import('./errors/access-denied/access-denied.module').then(m => m.AccessDeniedModule) },
     { path: 'not-found', loadChildren: () => import('./errors/not-found/not-found.module').then(m => m.NotFoundModule) },
